e285952d1fcf6723ce48f0a782f230ab701ad648
Sebastien Badia <seb@sebian.fr>
2012-10-15 00:35:37 +0200

    install gitolite from gitlabhq repos

--- manifests/gitolite.pp
+++ manifests/gitolite.pp
@@ -6,7 +6,7 @@ class gitlab::gitolite inherits gitlab::pre {
     '/var/cache/debconf/gitolite.preseed':
       ensure  => file,
       content => template('gitlab/gitolite.preseed.erb'),
-      before  => Package['gitolite'];
+      before  => Exec['install gitolite'];
     "${git_home}/${git_user}.pub":
       ensure  => file,
       owner   => $git_user,
@@ -19,7 +19,7 @@ class gitlab::gitolite inherits gitlab::pre {
       owner   => $git_user,
       group   => $git_user,
       mode    => '0644',
-      require => [Package['gitolite'],User[$git_user]];
+      require => [Exec['install gitolite'],User[$git_user]];
     "${git_home}/.gitolite/hooks/common/post-receive":
       ensure  => file,
       source  => 'puppet:///modules/gitlab/post-receive',
@@ -33,27 +33,33 @@ class gitlab::gitolite inherits gitlab::pre {
       owner   => $git_user,
       group   => $git_user,
       mode    => '0644',
-      require => Package['gitolite'];
+      require => User[$git_user];
     "${git_home}/.profile":
       ensure => file,
       source => 'puppet:///modules/gitlab/git_user-dot-profile',
       owner  => $git_user,
       group  => $git_user,
-      mode   => '0644';
-  }
-
-  package {
-    'gitolite':
-      ensure       => installed,
-      notify       => Exec['gl-setup gitolite'],
-      responsefile => '/var/cache/debconf/gitolite.preseed';
+      mode   => '0644',
+      notify => Exec['gl-setup gitolite'];
   }
 
   exec {
+    'clone gitolite':
+      command     => "git clone -b gl-v304 https://github.com/gitlabhq/gitolite.git ${git_home}/gitolite",
+      user        => $git_user,
+      require     => [File["${git_home}/.gitconfig"],File["${git_home}/${git_user}.pub"]],
+      path        => '/usr/bin/',
+      refreshonly => true;
+    'install gitolite':
+      command     => "${git_home}/gitolite/install -ln ${git_home}/bin",
+      user        => $git_user,
+      require     => Exec['clone gitolite'],
+      logoutput   => 'on_failure',
+      refreshonly => true;
     'gl-setup gitolite':
-      command     => "/bin/su -c '/usr/bin/gl-setup ${git_home}/${git_user}.pub > /dev/null 2>&1' ${git_user}",
-      user        => root,
-      require     => [Package['gitolite'],File["${git_home}/.gitconfig"],File["${git_home}/${git_user}.pub"]],
+      command     => "${git_home}/bin/gitolite setup -pk ${git_home}/${git_user}.pub",
+      user        => $git_user,
+      require     => [Package['install gitolite'],File["${git_home}/.gitconfig"],File["${git_home}/${git_user}.pub"]],
       logoutput   => 'on_failure',
       refreshonly => true;
   }
